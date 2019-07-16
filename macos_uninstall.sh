d31d5c4f423028104012f7719ad2b581a06a5d90
Truong Giang <tng2903@users.noreply.github.com>
2019-07-16 17:24:25 +0700

    Update for new version of Unity too

--- macos_uninstall.sh
+++ macos_uninstall.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
-
-#!/bin/bash
 for unity_dir in /Applications/Unity* 
+do
+	WRAPPER_LOCATION="${unity_dir}/Unity.app/Contents/Tools/PVRTexTool"
+	ORIGINAL_LOCATION="${unity_dir}/Unity.app/Contents/Tools/PVRTexTool_orig"
+
+	echo "Uninstalling from ${unity_dir}"
+
+	if [ -f "$ORIGINAL_LOCATION" ]; then
+		echo "Removing wrapper"
+		cp "$ORIGINAL_LOCATION" "$WRAPPER_LOCATION"
+		echo "Done"
+	else
+		echo "Original PVRTexTool not found. Doing nothing."
+	fi
+done
+
+for unity_dir in /Applications/Unity/Hub/Editor/*
 do
 	WRAPPER_LOCATION="${unity_dir}/Unity.app/Contents/Tools/PVRTexTool"
 	ORIGINAL_LOCATION="${unity_dir}/Unity.app/Contents/Tools/PVRTexTool_orig"
