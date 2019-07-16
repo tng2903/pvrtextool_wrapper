f3bb78cab943eb60d1c3416fb60eb93d3480747e
Truong Giang <tng2903@users.noreply.github.com>
2019-07-16 17:23:47 +0700

    Update for new version of Unity too

--- macos_install.sh
+++ macos_install.sh
@@ -20,3 +20,25 @@ do
 		fi
 	fi
 done
+
+for unity_dir in /Applications/Unity/Hub/Editor/*
+do
+	ORIGINAL_LOCATION="${unity_dir}/Unity.app/Contents/Tools/PVRTexTool"
+	COPY_OF_ORIGINAL_LOCATION="${unity_dir}/Unity.app/Contents/Tools/PVRTexTool_orig"
+
+	echo "Installing into ${unity_dir}"
+
+	if [ -f "$COPY_OF_ORIGINAL_LOCATION" ]; then
+		echo "Already installed here."
+	else
+		if [ -f "$ORIGINAL_LOCATION" ]; then
+			echo "Creating a copy of the original"
+			cp "${ORIGINAL_LOCATION}" "${COPY_OF_ORIGINAL_LOCATION}"
+			echo "Copying wrapper to ${COPY_OF_ORIGINAL_LOCATION}"
+			cp PVRTexTool "$ORIGINAL_LOCATION"
+			echo "Done"
+		else
+			echo "Unity PVRTexTool not found. Doing nothing."
+		fi
+	fi
+done
